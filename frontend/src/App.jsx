40939aa4d34e59d049a5cc0eca14c9f5ab57f892
sarthakdev143 <sarthakdev143.official@gmail.com>
2024-09-30 19:00:27 +0530

    frontend change to Vite from Next.js

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className='text-4xl text-white'>Hello World!</h1>
+    </main>
+  )
+}
+
+export default App
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
+
