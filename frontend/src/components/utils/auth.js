7a0f759e209b267903fee26925febf4eb0baad04
sarthakdev143 <sarthakdev143.official@gmail.com>
2024-11-06 16:43:12 +0530

    "Rename `NEXT_PUBLIC_API_BASE_URL` to `NEXT_PUBLIC_BACKEND_URL` in auth.js"

--- frontend/src/components/utils/auth.js
+++ frontend/src/components/utils/auth.js
@@ -48,7 +48,7 @@ export const AuthUtils = {
 
     createAuthenticatedAxios() {
         const instance = axios.create({
-            baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080',
+            baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8080',
             headers: {
                 'Content-Type': 'application/json'
             }
