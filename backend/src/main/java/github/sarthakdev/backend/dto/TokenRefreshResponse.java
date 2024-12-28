66017808fa770615113906a9b0c6eb699cd8ea4e
sarthakdev143 <sarthakdev143.official@gmail.com>
2024-12-29 00:01:33 +0530

    `Update TokenRefreshResponse class: rename accessToken to authToken and update constructor parameters`

--- backend/src/main/java/github/sarthakdev/backend/dto/TokenRefreshResponse.java
+++ backend/src/main/java/github/sarthakdev/backend/dto/TokenRefreshResponse.java
@@ -8,12 +8,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TokenRefreshResponse {
-    private String accessToken;
+    private String authToken;
     private String refreshToken;
     private String tokenType = "Bearer";
 
-    public TokenRefreshResponse(String accessToken, String refreshToken) {
-        this.accessToken = accessToken;
+    public TokenRefreshResponse(String authToken, String refreshToken) {
+        this.authToken = authToken;
         this.refreshToken = refreshToken;
     }
 }
